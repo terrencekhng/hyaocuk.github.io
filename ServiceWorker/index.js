28ac5ff27a7386c31eb7cc66fa2d24c345904e89
hyaocuk <awesome.terence@gmail.com>
2020-06-03 16:35:08 +0800

    fixed scope

--- ServiceWorker/index.js
+++ ServiceWorker/index.js
@@ -1,5 +1,12 @@
 if ( 'serviceWorker' in navigator ) {
     navigator.serviceWorker.register( 'sw.js' ).then( reg => {
+        if (reg.installing) {
+            console.log('Service Worker installing');
+        } else if (reg.waiting) {
+            console.log('Service Worker installed');
+        } else if (reg.active) {
+            console.log('Service Worker active');
+        }
         console.log( `Registration successed! Scope is ${reg.scope}` );
     } ).catch(error => {
         console.log( `Registration failed with ${error}` );
