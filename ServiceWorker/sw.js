0aba8d508cc082adc4a2bca254e44df65d2cb312
hyaocuk <awesome.terence@gmail.com>
2020-06-03 18:55:39 +0800

    update to v2 cache

--- ServiceWorker/sw.js
+++ ServiceWorker/sw.js
@@ -2,7 +2,7 @@ self.addEventListener( 'install', event => {
     /* Here we add an install event listener to the service worker (hence self), and then chain a ExtendableEvent.waitUntil() method onto the event — this ensures that the service worker will not install until the code inside waitUntil() has successfully occurred. */
     event.waitUntil(
         /* Inside waitUntil() we use the caches.open() method to create a new cache called v1, which will be version 1 of our site resources cache. This returns a promise for a created cache; once resolved, we then call a function that calls addAll() on the created cache, which for its parameter takes an array of origin-relative URLs to all the resources you want to cache. */
-        caches.open( 'v1' ).then( cache => {
+        caches.open( 'v2' ).then( cache => {
             return cache.addAll( [
                 './',
                 './index.html',
@@ -11,10 +11,6 @@ self.addEventListener( 'install', event => {
             ] )
         } )
     )
-} )
-
-self.addEventListener( 'activate', event => {
-    console.log( 'Activate succesfully!' );
 } )
 
 self.addEventListener( 'fetch', event => {
@@ -29,8 +25,22 @@ self.addEventListener( 'fetch', event => {
                     return response;
                 } );
             } )
-        } ).catch(() => {
-            return caches.match('404.html');
-        })
+        } ).catch( () => {
+            return caches.match( '404.html' );
+        } )
     )
 } )
+
+/* You also get an activate event. This is a generally used to do stuff that would have broken the previous version while it was still running, for example getting rid of old caches. This is also useful for removing data that is no longer needed to avoid filling up too much disk space — each browser has a hard limit on the amount of cache storage that a given service worker can use. The browser does its best to manage disk space, but it may delete the Cache storage for an origin.  The browser will generally delete all of the data for an origin or none of the data for an origin. */
+self.addEventListener( 'activate', event => {
+    let cacheKeepList = [ 'v2' ];
+    event.waitUntil(
+        caches.keys().then( keyList => {
+            return Promise.all( keyList.map( key => {
+                if ( cacheKeepList.indexOf( key ) === -1 ) {
+                    return caches.delete( key );
+                }
+            } ) );
+        } )
+    );
+} );
